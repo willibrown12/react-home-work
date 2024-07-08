4058c554da7f039af93e70dfb85453de44fafe2e
willibrown12 <viliking123@gmail.com>
2024-07-08 20:42:51 +0300

    homework

--- react practice/src/components/countries/index.tsx
+++ react practice/src/components/countries/index.tsx
@@ -1,18 +1,53 @@
+import { useState } from "react"
 import countries from "../../countries.json"
 import { Singlecountry } from "./single-country"
+
 
 type countrietype = typeof countries[0]
 
 export function Countrylist() {
-    return <div style={{display:"flex", flexWrap:"wrap"}}>
-        {countries.map((country: countrietype) => {
+    const [filter,setFilter]=useState("")
+    const [filterpop,setFilter2]=useState(false)
+ 
+   const countriesfilter = filter
+    ? countries.filter((country) =>
+        country.name.common.toLowerCase().includes(filter.toLowerCase())
+      )
+    : countries; 
+
+    const countriesfilter2 = filterpop
+    ? countriesfilter.filter((country) =>
+        country.population>10000000
+      )
+    : countriesfilter; 
+
+
+    return  <div style={{display:"flex", flexDirection:"column", justifyContent:"center",gap:"20px"}}> {  <input
+        type="text"
+        placeholder="search for country"
+        onChange={(e) => {
+          setFilter(e?.target?.value?.toLowerCase());
+        }}
+      /> }
+    <button onClick={()=>{
+            setFilter2(!filterpop)
+            
+           
+
+
+         }}>
+           filter pop higher 10m
+          </button>
+    <div style={{display:"flex", flexWrap:"wrap", gap:"50px"}}>
+        
+        {countriesfilter2.map((country: countrietype) => {
             return <Singlecountry  countryName={country.name.common}
             countrypicture={country.flags.svg}
             key={country.name.official}
             />
         })}
     </div>
+    </div>
 }
-
 
 
--- react practice/src/components/countries/single-country.tsx
+++ react practice/src/components/countries/single-country.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 type Props = {
     countryName: string,
@@ -6,10 +7,51 @@ type Props = {
 }
 
 export function Singlecountry(props: Props) {
-  
+    const [inputValue, setInputValue] = useState("");
+    const [post, setPost] = useState("");
+    const [comment, addComment] = useState(false)
+    
+   
+
     return <div style={{ border: "1px dashed black", width:"350px" }}>
         <h2 >{props.countryName}</h2>
       <img src={props.countrypicture} alt="" style={{width:"300px"}}/>
+      {comment ? (
+        <div>
+             <input
+        value={inputValue}
+        type="text"
+        id="input"
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+         <button onClick={()=>{
+            setPost(inputValue)
+            
+           
+
+
+         }}>
+           post
+          </button>
+        </div>
+         
+        ) : (
+            
+          <button
+            onClick={() => {
+                addComment(true);
+            }}
+          >
+           add comment
+          </button>
+        )}
+        <div>{post}</div>
 
     </div>
 }
+
+
+
+
